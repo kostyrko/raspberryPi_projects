7a9d6e752faa0ba5a1495405d22187b9dc1d07e6
kostyrko <mkostyrko@gmail.com>
2020-01-03 17:58:56 +0100

    Add camera image effects test

---
+++ camera_module/camera_image_effects_test.py
@@ -0,0 +1,19 @@
+''' simple  script testing possible image effects
+for Raspberry Pi camera module, saves filtered images as jpg'''
+
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+# begain camera preview with opacity of 180 (max. 255)
+camera.start_preview(alpha=180)
+
+for effect in camera.IMAGE_EFFECTS:
+    camera.image_effect = effect
+    #text description on the top of an image
+    camera.annotate_text = "Effect: %s" % effect
+    sleep(2)
+    camera.capture('/home/pi/Desktop/image_%s.jpg' % effect)
+# after the loop is finished end preview
+camera.stop_preview()
